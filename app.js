61f07bcba3f2e9c92bec5b3d0845bba25c1aec2c
Nathaniel Gibson <89817052+gibs0nnn@users.noreply.github.com>
2024-06-26 13:01:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// Getting elements from the DOM
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
+
+let selectedRating = "Satisfied";
+
+// Attach events to ratingsContainer
+ratingsContainer.addEventListener("click", e => {
+    if (e.target.parentNode.classList.contains("rating")) {
+        removeActive();
+        e.target.parentNode.classList.add("rating");
+        selectedRating = e.target.nextElementSibling.innerHTML;
+    }
+});
+
+sendBtn.addEventListener("click", () => {
+    panel.innerHTML = `
+      <p class="heart">💖</p>
+      <strong>Thank You! </strong>
+      <br>
+      <strong>Feedback: ${selectedRating} </strong>
+    `;
+  });
+
+  function removeActive() {
+    for (let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove("active");
+    }
+  }
